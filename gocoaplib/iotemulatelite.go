190173fc3f63320d3cb3701fba9bf2d206d96d5d
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-06-07 20:54:11 +0300

    trying to build system with switcher and observ checking

---
+++ gocoaplib/iotemulatelite.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/plgd-dev/go-coap/v2/udp"
+	"github.com/plgd-dev/go-coap/v2/udp/message/pool"
+)
+
+func main() {
+	sync := make(chan bool)
+
+	co, err := udp.Dial("localhost:5688")
+	if err != nil {
+		log.Fatalf("Error dialing: %v", err)
+	}
+
+	num := 0
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	obs, err := co.Observe(ctx, "/some/path", func(req *pool.Message) {
+		log.Printf("Got %+v\n", req)
+		num++
+		if num >= 10 {
+			sync <- true
+		}
+	})
+	if err != nil {
+		log.Fatalf("Unexpected error '%v'", err)
+	}
+
+	<-sync
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	obs.Cancel(ctx)
+}
