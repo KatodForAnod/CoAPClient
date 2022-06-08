d094f1f41ac128bedeff0c5fb941a5ff7be43c77
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-06-08 15:12:28 +0300

    added some funcs for check observe proc

--- pkg/iot/client.go
+++ pkg/iot/client.go
@@ -7,17 +7,23 @@ import (
 	"github.com/plgd-dev/go-coap/v2/udp/message/pool"
 	"golang.org/x/net/context"
 	"log"
+	"time"
 )
 
 type IoTDevice struct {
 	addr string
 	name string
 	conn *client.ClientConn
+
+	observe                *client.Observation
+	isObserveInformProcess *bool
 }
 
 func (d *IoTDevice) Init(config config.IotConfig) {
 	d.addr = config.Addr
 	d.name = config.Name
+
+	d.isObserveInformProcess = new(bool)
 }
 
 func (d *IoTDevice) Ping(ctx context.Context) error {
@@ -33,13 +39,34 @@ func (d *IoTDevice) Ping(ctx context.Context) error {
 func (d *IoTDevice) ObserveInform(ctx context.Context,
 	processMsg func(req *pool.Message)) error {
 	log.Println("observe information iot", d.name)
+	b := true
+	d.isObserveInformProcess = &b
 	observe, err := d.conn.Observe(ctx, "/some/path", processMsg)
 	if err != nil {
 		log.Println(observe)
+		b := false //check change
+		d.isObserveInformProcess = &b
 		return err
 	}
 
 	return nil
+}
+
+func (d *IoTDevice) StopObserveInform() error {
+	b := false
+	d.isObserveInformProcess = &b
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	if err := d.observe.Cancel(ctx); err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
+
+func (d *IoTDevice) IsObserveInformProcess() bool {
+	return *d.isObserveInformProcess
 }
 
 func (d *IoTDevice) Connect() error {
