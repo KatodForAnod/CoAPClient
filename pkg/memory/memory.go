617bc738f6cf0f6ec0b48bfb10b3007dab48ccd4
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-06-08 15:11:01 +0300

    sample of fmt memory

--- pkg/memory/memory.go
+++ pkg/memory/memory.go
@@ -1,7 +1,17 @@
 package memory
 
-import "github.com/plgd-dev/go-coap/v2/message"
+import (
+	"fmt"
+	"github.com/plgd-dev/go-coap/v2/message"
+)
 
 type Memory interface {
 	Save([]byte, message.MediaType) error
+}
+
+type MemoryFmt struct{}
+
+func (f MemoryFmt) Save(msg []byte, typeMsg message.MediaType) error {
+	fmt.Println(string(msg), typeMsg.String())
+	return nil
 }
