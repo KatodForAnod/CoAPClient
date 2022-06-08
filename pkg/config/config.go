3d02289e94d92131e60e6eaed2d38ef6aece4111
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-06-08 15:19:18 +0300

    conf load func sample

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,9 +1,19 @@
 package config
 
 type Config struct {
+	IoTsDevices []IotConfig `json:"io_ts_devices"`
 }
 
 type IotConfig struct {
-	Addr string
-	Name string
+	Addr string `json:"addr"`
+	Name string `json:"name"`
+}
+
+func LoadConfig() (Config, error) {
+	conf := Config{IoTsDevices: []IotConfig{{
+		Addr: "localhost:5688",
+		Name: "testDevice",
+	}}}
+
+	return conf, nil
 }
