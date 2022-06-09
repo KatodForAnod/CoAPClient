af11715e95d769dd6cb19f682ace0f1ca56c1243
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-06-09 09:32:13 +0300

    server controller sample

--- pkg/controller/server_controller.go
+++ pkg/controller/server_controller.go
@@ -1,1 +1,19 @@
 package controller
+
+import (
+	"CoAPClient/pkg/config"
+	"CoAPClient/pkg/memory"
+)
+
+type Controller struct {
+	mem memory.Memory
+}
+
+func (c *Controller) InitStruct(config config.Config, mem memory.Memory) {
+	c.mem = mem
+}
+
+// args to know what type info must recieve?
+func (c *Controller) GetInformation() ([]byte, error) {
+	return c.mem.Load()
+}
