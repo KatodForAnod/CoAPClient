1d11de05719c2e86452ffe66911a38e91669e8bd
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-06-09 09:44:13 +0300

    sample of proxy server

--- pkg/proxy_server.go
+++ pkg/proxy_server.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"CoAPClient/pkg/config"
-	"CoAPClient/pkg/iot"
+	"CoAPClient/pkg/controller"
+	"log"
+	"net/http"
 )
 
 type Server struct {
-	ioTDevices []iot.IoTDevice // TODO wrap into interface
-	//connection to Another server
+	controller controller.Controller
 }
 
-func (s *Server) StartServer(config config.Config) {
+func (s *Server) getInformationFromIotDevice(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	inf, err := s.controller.GetInformation()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "internal error", http.StatusInternalServerError)
+		return
+	}
 
+	_, err = w.Write(inf)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func (s *Server) StartServer(config config.Config, controller controller.Controller) {
+	s.controller = controller
+
+	http.HandleFunc("/", s.getInformationFromIotDevice)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
