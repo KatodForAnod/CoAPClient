2e3793bf5f9b6f3c60a8eb6d2c33e54f499e4490
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-06-09 17:17:29 +0300

    new funcs add and remove iot devices

--- pkg/controller/server_controller.go
+++ pkg/controller/server_controller.go
@@ -2,12 +2,14 @@ package controller
 
 import (
 	"CoAPProxyServer/pkg/config"
+	"CoAPProxyServer/pkg/iot"
 	"CoAPProxyServer/pkg/memory"
 	"log"
 )
 
 type Controller struct {
-	mem memory.Memory
+	mem            memory.Memory
+	ioTsController IoTsController
 }
 
 func (c *Controller) InitStruct(config config.Config, mem memory.Memory) {
@@ -24,4 +26,32 @@ func (c *Controller) GetInformation(deviceName string) ([]byte, error) {
 	}
 
 	return load, nil
+}
+
+func (c *Controller) NewIotDeviceObserve(iotConfig config.IotConfig) error {
+	log.Println("controller new iotDevicesObserve")
+	iotDev := iot.IoTDevice{}
+	iotDev.Init(iotConfig)
+	var arr []*iot.IoTDevice
+	arr = append(arr, &iotDev)
+
+	err := c.ioTsController.AddIoTs(arr)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	err = c.ioTsController.StartInformationCollect()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func (c *Controller) RemoveIoTDeviceObserve(ioTsConfig []config.IotConfig) error {
+	log.Println("controller remove ioTDeviceObserve")
+	c.ioTsController.RemoveIoTs(ioTsConfig)
+	return nil
 }
