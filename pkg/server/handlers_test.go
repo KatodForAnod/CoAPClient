b6d22edbf4b73b8f492b26d6211449f0ac5032eb
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-07-01 13:26:22 +0300

    added test for log handler

--- pkg/server/handlers_test.go
+++ pkg/server/handlers_test.go
@@ -3,12 +3,17 @@ package server
 import (
 	"CoAPProxyServer/pkg/config"
 	"errors"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
 var (
 	proxyServer Server
 )
+
+const serverAddr = "127.0.0.1:8080"
 
 type Controller struct {
 	ioTs []config.IotConfig
@@ -42,7 +47,7 @@ func (c *Controller) GetInformation(deviceName string) ([]byte, error) {
 
 func Init() {
 	controller := Controller{}
-	proxyServer.controller = &controller
+	proxyServer.StartServer(config.Config{ProxyServerAddr: serverAddr}, &controller)
 }
 
 func TestServer_getInformationFromIotDevice(t *testing.T) {
@@ -58,5 +63,17 @@ func TestServer_removeIotDevice(t *testing.T) {
 }
 
 func TestServer_getLogs(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/logs?countLogs=2", nil)
+	w := httptest.NewRecorder()
+	proxyServer.getLogs(w, req)
 
+	if want, got := http.StatusOK, w.Result().StatusCode; want != got {
+		t.Fatalf("expected a %d, instead got: %d", want, got)
+	}
+
+	out := w.Body.String()
+	outArr := strings.Split(out, "\n")
+	if len(outArr) < 2 {
+		t.FailNow()
+	}
 }
